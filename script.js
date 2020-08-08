c5ba92a6dd93b8bd6f806fb283fc8517c786ede9
uchihaNayeem <uchiha.nayeem@gmail.com>
2020-08-08 09:33:28 +0600

    jSON

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-console.log(`hello 25`);
+
+//25.3
+//what is api
+
+//https://www.youtube.com/watch?v=s7wmiS2mSXY
+
+
+/* --- END --- */
+
+
+
+//25.4 JSON, JSON STRUCTURE, PARSE, STRINGIFY, JSON PROPERTIES
+
+const workers = {
+  id: 12, 
+  name:`kolim`,
+  lover: {
+    name: `nishi`,
+    id: 12,
+  },
+  working: [12, 23, `worker`]
+};
+
+const workerJsonStringify = JSON.stringify(workers);
+console.log(workerJsonStringify); // {"id":12,"name":"kolim"}
+
+
+const workerJsonPerse = JSON.parse(workerJsonStringify);
+console.log(workerJsonPerse);
+
+
+
+
+
+/* --- END --- */
+//25.2 
+//APPLY MAP, FILTER, FIND ON AN ARRAY OF OBJECTS
+
+
+/* --- END --- */
+//25.2 
+//APPLY MAP, FILTER, FIND ON AN ARRAY OF OBJECTS
+
+
+/* --- END --- */
